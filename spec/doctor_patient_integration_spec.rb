7d22e2605c65c7dce5443d6424b2d386307a5109
Meaghan Jones and Maldon Meehan <meaghan@>
2016-07-12 16:21:30 -0700

    add a new doctor path passing spec

--- spec/doctor_patient_integration_spec.rb
+++ spec/doctor_patient_integration_spec.rb
@@ -16,17 +16,28 @@ describe("add a new doctor", {:type => :feature}) do
   end
 end
 
-describe("add a new patient to a doctor path", {:type => :feature}) do
-  it ("allows an admistrator to create a new patient to a doctor") do
+describe("add a new doctor path", {:type => :feature}) do
+  it ("allows an admistrator to view a new doctor") do
     doctor = Doctor.new({:name => 'Dr. Smith', :specialty => 'Heart', :id => nil})
     doctor.save()
     visit('/doctors')
   end
 end
 
-# click_link('Dr. Smith')
-# click_link('Add a Patient')
-# fill_in('patient_name', :with => 'Andrea')
-# fill_in('birthdate', :with => '1999-08-01')
-# click_button('Submit')
-# expect(page).to have_content('Your entry has been successfully created!')
+describe("add a new patient path", {:type => :feature}) do
+  it ("allows an admistrator to a new patient") do
+    doctor = Doctor.new({:name => 'Dr. Smith', :specialty => 'Heart', :id => nil})
+    doctor.save()
+    visit('/doctors')
+    click_link('Dr. Smith')
+  end
+end
+
+#
+#     click_link('Add a Patient')
+#     fill_in('patient_name', :with => 'Andrea')
+#     fill_in('birthdate', :with => '1999-08-01')
+#     click_button('Submit')
+#     expect(page).to have_content('Your entry has been successfully created!')
+#   end
+# end
